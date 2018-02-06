b82789b15ee85290152dc2086680154b11fe06e2
Tristan Bennett <tmbennett2011@msn.com>
2018-02-06 08:17:16 -0700

    added button for color change

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 		<h1>Literary Mutilator</h1>
 		<h2>JavaScript Events</h2>
 
+
+
 		<label for="search">Find:</label>
 		<input id="search" name="search" type="text">
 		<br>
@@ -26,5 +28,7 @@
 
 			<p id="para3">Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores. At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles. Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan</p>
 		</div>
+		<label for="color change"><strong>Change Color</strong></label>
+		<input type="button" value="color change" name="color change"  onclick="colorChange();">
 	</body>
 </html>
